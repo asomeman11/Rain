21d0d0a96857844a3aa2270b4ac660352a12611b
Paul Burkhalter <jedipbb1383@gmail.com>
2016-07-24 13:01:09 -0500

    Collision Sliding
    Added the ability to 'slide' while colliding with an object.

--- src/com/burksnet/code/games/rain/entity/mob/Mob.java
+++ src/com/burksnet/code/games/rain/entity/mob/Mob.java
@@ -24,6 +24,12 @@ public abstract class Mob extends Entity {
 
 	public void move(int xa, int ya) {
 
+		if(xa != 0 && ya != 0){
+			move(xa, 0);
+			move(0, ya);
+			return;
+		}
+		
 		if (xa > 0)
 			dir = Direction.EAST;
 		if (xa < 0)
