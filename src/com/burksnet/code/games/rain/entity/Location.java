1dbc729e781f909eac2c9d092a59cc0f50f65e8b
Paul Burkhalter <jedipbb1383@gmail.com>
2016-07-25 16:27:29 -0500

    Location Class
    Added the location class. Represents a location on the map.

---
+++ src/com/burksnet/code/games/rain/entity/Location.java
@@ -0,0 +1,26 @@
+package com.burksnet.code.games.rain.entity;
+
+public class Location {
+
+	private int x, y;
+	
+	public Location(int xx, int yy){
+		x = xx;
+		y = yy;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public int[] getXY(){
+		return new int[]{
+			x,y
+		};
+	}
+	
+}
